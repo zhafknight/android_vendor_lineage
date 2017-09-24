4aa2985032586b4710e7650f4ac17f6d47c630fa
Dominggoes Isakh <drjisakh@gmail.com>
2017-09-24 22:28:31 +0200

    soong_config: Add BOARD_EGL_WORKAROUND_BUG_10194508
         * Soong product variable 'Egl_workaround_bug_10194508'
    
    Change-Id: I7f845ca15ee7ba9d6224ba25e1003bac21feefb2

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,5 +1,8 @@
 package android
 type Product_variables struct {
+	Egl_workaround_bug_10194508 struct {
+		Cppflags []string
+	}
 	Exynos4_enhancements struct {
 		Cflags []string
 		Cppflags []string
@@ -32,6 +35,7 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
+	Egl_workaround_bug_10194508    *bool `json:",omitempty"`
 	Exynos4_enhancements    *bool `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Needs_legacy_camera_hal1_dyn_native_handle  *bool `json:",omitempty"`
