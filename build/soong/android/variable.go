f39bc6be68ad1782d968e5902c8e9ceb287763de
Dominggoes Isakh <drjisakh@gmail.com>
2017-11-16 08:24:01 +0100

    soong_config: Add BOARD_EGL_NEEDS_HANDLE_VALUE
         * Soong product variable 'Egl_needs_handle_value'
    
    Change-Id: I289bf531aa61b04997e4b0f4a9ef8a23ab6f78ff

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,5 +1,8 @@
 package android
 type Product_variables struct {
+	Egl_needs_handle struct {
+		Cppflags []string
+	}
 	Egl_workaround_bug_10194508 struct {
 		Cppflags []string
 	}
@@ -52,6 +55,7 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
+	Egl_needs_handle    *bool `json:",omitempty"`
 	Egl_workaround_bug_10194508    *bool `json:",omitempty"`
 	Exynos4_enhancements    *bool `json:",omitempty"`
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
