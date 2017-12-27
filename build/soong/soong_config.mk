6aa8d1553f01af43ea6fc059168d1739188c6ac4
Dominggoes Isakh <drjisakh@gmail.com>
2017-12-27 11:37:34 +0100

    soong_config: Add TARGET_DISABLE_ASHMEM_TRACKING
             * Soong product variable 'Disable_ashmem_tracking'
    
    Change-Id: I690d3d249bf140050f2066787b82d230724f61b2

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,6 +4,7 @@ lineage_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Lineage": {'; \
+	echo '    "Disable_ashmem_tracking": $(if $(filter true,$(TARGET_DISABLE_ASHMEM_TRACKING)),true,false),'; \
 	echo '    "Allows_invalid_pthread": $(if $(filter true,$(TARGET_ALLOWS_INVALID_PTHREAD)),true,false),'; \
 	echo '    "Egl_needs_handle": $(if $(filter true,$(BOARD_EGL_NEEDS_HANDLE_VALUE)),true,false),'; \
 	echo '    "Egl_workaround_bug_10194508": $(if $(filter true,$(BOARD_EGL_WORKAROUND_BUG_10194508)),true,false),'; \
